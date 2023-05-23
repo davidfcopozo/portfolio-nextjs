f16ae2428094e48c64cc5925d534d71654d8ef77
David Francisco <davidfco.pozo@gmail.com>
2023-05-23 22:23:54 +0200

    Broken images on deploy fixed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -133,82 +133,83 @@ function Projects() {
         <h2>Projects</h2>
       </motion.div>
       <div className="projects-container">
-        {projectsData.map(
-          (
-            {
-              image,
-              projectDescription,
-              projectLink,
-              projectExternalLinks,
-              projectName,
-              projectTech,
-            },
-            index
-          ) => {
-            return (
-              <motion.div
-                className="project"
-                key={`${index}-${projectName}`}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                transition={{ duration: 0.6 }}
-                variants={{
-                  visible: { opacity: 1, y: -50 },
-                  hidden: { opacity: 0, y: 0 },
-                }}
-              >
-                <div className="project-image">
-                  <div className="project-image-overlay"></div>
-                  <div className="project-image-container">
-                    <Image
-                      src={`${image}`}
-                      fill
-                      alt={projectName}
-                      quality={100}
-                      style={{ borderRadius: "5px" }}
-                    />
+        {projectsData &&
+          projectsData.map(
+            (
+              {
+                image,
+                projectDescription,
+                projectLink,
+                projectExternalLinks,
+                projectName,
+                projectTech,
+              },
+              index
+            ) => {
+              return (
+                <motion.div
+                  className="project"
+                  key={`${index}-${projectName}`}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  transition={{ duration: 0.6 }}
+                  variants={{
+                    visible: { opacity: 1, y: -50 },
+                    hidden: { opacity: 0, y: 0 },
+                  }}
+                >
+                  <div className="project-image">
+                    <div className="project-image-overlay"></div>
+                    <div className="project-image-container">
+                      <Image
+                        src={`${image}`}
+                        fill
+                        alt={projectName}
+                        quality={100}
+                        style={{ borderRadius: "5px" }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="project-info">
-                  <h3 className="project-info-title">{projectName}</h3>
-                  <div className="project-info-description">
-                    <p>{projectDescription}</p>
-                  </div>
-                  <ul className="project-info-tech-list">
-                    {projectTech.map((tech) => (
-                      <li className="project-info-tech-list-item" key={tech}>
-                        {tech}
+                  <div className="project-info">
+                    <h3 className="project-info-title">{projectName}</h3>
+                    <div className="project-info-description">
+                      <p>{projectDescription}</p>
+                    </div>
+                    <ul className="project-info-tech-list">
+                      {projectTech.map((tech) => (
+                        <li className="project-info-tech-list-item" key={tech}>
+                          {tech}
+                        </li>
+                      ))}
+                    </ul>
+                    <ul className="project-info-links">
+                      <li className="project-info-links-item">
+                        {projectExternalLinks.github && (
+                          <Link
+                            href={projectExternalLinks.github}
+                            className="project-info-links-item-link"
+                          >
+                            <FiGithub />
+                          </Link>
+                        )}
                       </li>
-                    ))}
-                  </ul>
-                  <ul className="project-info-links">
-                    <li className="project-info-links-item">
-                      {projectExternalLinks.github && (
-                        <Link
-                          href={projectExternalLinks.github}
-                          className="project-info-links-item-link"
-                        >
-                          <FiGithub />
-                        </Link>
-                      )}
-                    </li>
-                    <li className="project-info-links-item">
-                      {projectExternalLinks.externalLink && (
-                        <Link
-                          href={projectExternalLinks.externalLink}
-                          className="project-info-links-item-link"
-                        >
-                          <FiExternalLink />
-                        </Link>
-                      )}
-                    </li>
-                  </ul>
-                </div>
-              </motion.div>
-            );
-          }
-        )}
+                      <li className="project-info-links-item">
+                        {projectExternalLinks.externalLink && (
+                          <Link
+                            href={projectExternalLinks.externalLink}
+                            className="project-info-links-item-link"
+                          >
+                            <FiExternalLink />
+                          </Link>
+                        )}
+                      </li>
+                    </ul>
+                  </div>
+                </motion.div>
+              );
+            }
+          )}
       </div>
     </section>
   );
