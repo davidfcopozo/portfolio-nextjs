d4e21d582cd32d9e14996c0472a5ba6f70e6469f
David Francisco <davidfco.pozo@gmail.com>
2023-05-23 21:56:26 +0200

    Broken images on deploy fixed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "https://www.davidfrancisco.dev/",
+      "davidfrancisco.dev",
+      "portafolio-eta-ten.vercel.app",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
