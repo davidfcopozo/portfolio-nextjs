470bc378d5752dbb96a29f30ccd36533aec6beda
David Francisco <davidfco.pozo@gmail.com>
2023-05-24 10:32:30 +0200

    Restored next.config.js file
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "https://www.davidfrancisco.dev/",
-      "davidfrancisco.dev",
-      "portafolio-eta-ten.vercel.app",
-    ],
-  },
-  output: "standalone",
-  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
