3d1b2a017e6fe655b6578db4b1f5f7625e77684f
David Francisco <davidfco.pozo@gmail.com>
2023-05-29 22:44:08 +0200

    Added validations and modulirized submit funcction
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,63 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import TextError from "../components/TextError";
 import yupContactFormValidation from "@/helpers/yupContactFormValidation";
+import useFormSubmit from "@/hooks/useFormSubmit";
+import LoadingIndicator from "./LoadingIndicator";
 
 interface Values {
   name: string;
-  cel: number;
   email?: string;
   details?: string;
 }
 
-type SubmitFormProps = {
-  values: Values;
-  onSubmitProps: FormikHelpers<Values>;
-};
-
 const ContactForm = () => {
-  const [data, setData] = useState("");
-  const [success, setSuccess] = useState("");
-  const [error, setError] = useState("");
-  const [fail, setFail] = useState("");
   const { contactFormInitialValues, contactFormValidationSchema } =
     yupContactFormValidation();
+  const { submitData } = useFormSubmit();
 
-  /*   const styles = {
-    marginInline: "auto",
-    width: "80%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  }; */
-
-  const submitForm = (values: Values, onSubmitProps: FormikHelpers<Values>) => {
-    fetch("https://formsubmit.co/ajax/davidfco.pozo@gmail.com", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        NName: values.name,
-        Phone: values.cel,
-        Email: values.email,
-        Details: values.details,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        onSubmitProps.setSubmitting(false);
-        onSubmitProps.resetForm();
-        setData(data);
-        setSuccess(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
-        setFail(error);
-      });
+  const submitForm = async (
+    values: Values,
+    onSubmitProps: FormikHelpers<Values>
+  ) => {
+    await submitData(values, onSubmitProps);
   };
 
   return (
@@ -73,94 +36,76 @@ const ContactForm = () => {
           const clickHandler = () => {
             formik.setTouched({
               name: true,
-              cel: true,
               email: true,
               details: true,
             });
           };
 
           return (
-            <div className="form-container">
-              <div className="form-card"></div>
-              <Form className="form">
-                <div className="form-control">
-                  <label className="form-inputLabel" htmlFor="name">
-                    Name:
-                  </label>
-                  <Field
-                    className="form-inputField"
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Jon Doe"
-                    required
-                  />
-                  <ErrorMessage name="name" component={TextError} />
-                </div>
+            <Form className="form">
+              <div className="name">
+                <label className="form-input-label" htmlFor="name">
+                  Name:
+                </label>
+                <Field
+                  className="form-input-field"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Jon Doe"
+                  required
+                />
+                <ErrorMessage name="name" component={TextError} />
+              </div>
 
-                <div className="form-control">
-                  <label className="form-inputLabel" htmlFor="cel">
-                    Phone Number:
-                  </label>
-                  <Field
-                    className="form-inputField"
-                    type="tel"
-                    name="cel"
-                    id="cel"
-                    placeholder="976 876 876"
-                    required
-                  />
-                  <ErrorMessage name="cel" component={TextError} />
-                </div>
+              <div className="email">
+                <label className="form-input-label" htmlFor="email">
+                  E-Mail:
+                </label>
+                <Field
+                  className="form-input-field"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email@example.com"
+                  required
+                />
+                <ErrorMessage name="email" component={TextError} />
+              </div>
 
-                <div className="form-control">
-                  <label className="form-inputLabel" htmlFor="email">
-                    E-Mail:
-                  </label>
-                  <Field
-                    className="form-inputField"
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="myemail@example.com"
-                    required
-                  />
-                  <ErrorMessage name="email" component={TextError} />
-                </div>
+              <div className="details">
+                <label className="form-input-label" htmlFor="details">
+                  Details:
+                </label>
+                <Field
+                  className="form-input-field"
+                  as="textarea"
+                  name="details"
+                  id="details"
+                  placeholder="Type a message..."
+                  required
+                />
+                <ErrorMessage name="details" component={TextError} />
+              </div>
 
-                <div className="form-control">
-                  <label className="form-inputLabel" htmlFor="details">
-                    Details:
-                  </label>
-                  <Field
-                    className="form-inputField"
-                    as="textarea"
-                    name="details"
-                    id="details"
-                    placeholder="Information"
-                    required
-                  />
-                  <ErrorMessage name="details" component={TextError} />
-                </div>
-
-                <button
-                  className="action-btn"
-                  type="submit"
-                  onClick={clickHandler}
-                  disabled={
-                    !formik.isValid || formik.isSubmitting ? true : false
-                  }
-                  style={{
-                    backgroundColor:
-                      !formik.isValid || formik.isSubmitting
-                        ? "gray"
-                        : "inherit",
-                  }}
-                >
-                  Send
-                </button>
-              </Form>
-            </div>
+              <button
+                className="action-btn"
+                type="submit"
+                onClick={clickHandler}
+                disabled={!formik.isValid || formik.isSubmitting ? true : false}
+                style={{
+                  backgroundColor:
+                    !formik.isValid || formik.isSubmitting ? "gray" : "#5596bf",
+                  color:
+                    !formik.isValid || formik.isSubmitting
+                      ? "white"
+                      : "#0a192f",
+                }}
+              >
+                Send
+              </button>
+              {formik.isSubmitting && <LoadingIndicator />}
+            </Form>
           );
         }}
       </Formik>
