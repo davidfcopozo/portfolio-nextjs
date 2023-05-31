99d1796526ffca5090d87b3486429005c7068415
David Francisco <davidfco.pozo@gmail.com>
2023-05-31 13:51:17 +0200

    Form validation added
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/helpers/yupContactFormValidation.tsx
+++ src/helpers/yupContactFormValidation.tsx
@@ -3,17 +3,12 @@ import * as Yup from "yup";
 const yupContactFormValidation = () => {
   const contactFormInitialValues = {
     name: "",
-    cel: 0,
     email: "",
     details: "",
   };
 
   const contactFormValidationSchema = Yup.object({
     name: Yup.string(),
-    cel: Yup.number()
-      .typeError("This field only takes numbers.")
-      .min(9, "Incomplete phone number.")
-      .integer(),
     email: Yup.string()
       .email("Invalid email address.")
       .required("This field is required."),
