88b812969c5f3b288be523ebc4a5faaa7aac73f3
David <davidfco.pozo@gmail.com>
2025-01-27 20:14:22 +0100

    Add multilingual support to contact form validation
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- src/helpers/yupContactFormValidation.tsx
+++ src/helpers/yupContactFormValidation.tsx
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-const yupContactFormValidation = () => {
+const yupContactFormValidation = ({ language }: { language: "en" | "es" }) => {
   const contactFormInitialValues = {
     name: "",
     email: "",
@@ -8,11 +8,27 @@ const yupContactFormValidation = () => {
   };
 
   const contactFormValidationSchema = Yup.object({
-    name: Yup.string().required("This field is required."),
+    name: Yup.string().required(
+      language === "en"
+        ? "This field is required."
+        : "Este campo es obligatorio."
+    ),
     email: Yup.string()
-      .email("Invalid email address.")
-      .required("This field is required."),
-    details: Yup.string().required("Please, type a message."),
+      .email(
+        language === "en"
+          ? "Invalid email address."
+          : "Correo electrónico inválido."
+      )
+      .required(
+        language === "en"
+          ? "This field is required."
+          : "Este campo es obligatorio."
+      ),
+    details: Yup.string().required(
+      language === "en"
+        ? "Please, type a message."
+        : "Por favor, escribe un mensaje."
+    ),
   });
 
   return {
